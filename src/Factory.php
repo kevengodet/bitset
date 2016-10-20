e872164d1e21c0bbbce5dffc27cdc538ca5a2002
Keven <keven@adagiolabs.fr>
2016-10-20 10:30:35 +0200

    Fixed Factory::fromArray()

--- src/Factory.php
+++ src/Factory.php
@@ -51,7 +51,7 @@ final class Factory
     {
         $class = self::findBestClass();
 
-        return $class::fromString($inputArray);
+        return $class::fromArray($inputArray);
     }
 
     /**
